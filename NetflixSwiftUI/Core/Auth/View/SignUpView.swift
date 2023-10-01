35d3ad89a17e33d92aed899e637713c2bff9da84
N3v1 <n3v1010@gmail.com>
2023-10-01 13:34:24 +0200

    Update SignUpView.swift

--- NetflixSwiftUI/Core/Auth/View/SignUpView.swift
+++ NetflixSwiftUI/Core/Auth/View/SignUpView.swift
@@ -12,9 +12,53 @@ struct SignUpView: View {
     @State private var password = ""
     @State private var username = ""
     @State private var fullname = ""
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("netflix-wallpaper")
+                .resizable()
+                .scaledToFill()
+                .overlay {
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.6)
+                }
+            VStack {
+                Spacer()
+                // MARK: - Logo
+                Image("netflix logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .padding()
+                
+                // MARK: - Login
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocorrectionDisabled()
+                        .modifier(IGTextFieldModifier())
+                        .frame(width: 400, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .autocorrectionDisabled()
+                        .modifier(IGTextFieldModifier())
+                        .frame(width: 400, height: 100)
+                    
+                    TextField("Enter your full name", text: $fullname)
+                        .modifier(IGTextFieldModifier())
+                        .frame(width: 400, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                    
+                    TextField("Enter an username", text: $username)
+                        .modifier(IGTextFieldModifier())
+                        .frame(width: 400, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                }
+                
+            }
+        }
     }
 }
 
+//#Preview {
+//    SignUpView()
+//}
