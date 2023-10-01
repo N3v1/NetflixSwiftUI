7f0d304938fbe1bbc2407d8bef2530bb43f65f5c
N3v1 <n3v1010@gmail.com>
2023-10-01 10:39:36 +0200

    create login view

---
+++ NetflixSwiftUI/Core/Auth/View/LogInView.swift
@@ -0,0 +1,16 @@
+//
+//  LogInView.swift
+//  NetflixSwiftUI
+//
+//  Created by Nevio Hirani on 01.10.23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
