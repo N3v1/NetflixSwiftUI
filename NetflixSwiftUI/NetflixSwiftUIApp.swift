1daef0b68fad71556a541c634a5cbb58bb88ddf0
N3v1 <n3v1010@gmail.com>
2023-10-01 10:22:09 +0200

    Initial Commit

---
+++ NetflixSwiftUI/NetflixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSwiftUIApp.swift
+//  NetflixSwiftUI
+//
+//  Created by Nevio Hirani on 01.10.23.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
