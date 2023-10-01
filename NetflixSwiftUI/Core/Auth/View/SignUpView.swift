5ebc080db173d3bc2ccfc8d710a7fb1b32a2b665
N3v1 <n3v1010@gmail.com>
2023-10-01 10:39:02 +0200

    create signup view

---
+++ NetflixSwiftUI/Core/Auth/View/SignUpView.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpView.swift
+//  NetflixSwiftUI
+//
+//  Created by Nevio Hirani on 01.10.23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
